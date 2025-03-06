cfc65e56163531671c82de19d56957936fbb11c1
Devansh Bhardwaj <dbhardwaj123411@gmail.com>
2025-03-06 20:12:20 +0530

    url

--- crawl.js
+++ crawl.js
@@ -1,3 +1,32 @@
+const { JSDOM } = require("jsdom")
+
+function getURLFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll("a")
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === "/") {
+            //relative
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with relative url: ${err.message}`);
+            }
+        }
+        else {
+            //absolute
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log(`error with absolute url: ${err.message}`);
+            }
+        }
+
+    }
+    return urls
+}
 
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
@@ -8,4 +37,4 @@ function normalizeURL(urlString) {
     return hostPath;
 }
 
-module.exports = { normalizeURL };
+module.exports = { normalizeURL, getURLFromHTML };
